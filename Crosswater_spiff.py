744bc795521c02f1a64286b983b13316d8db1b1b
awaddell77 <waddell.andrew@gmail.com>
2018-09-19 16:21:45 -0700

    Updates

--- Crosswater_spiff.py
+++ Crosswater_spiff.py
@@ -5,6 +5,16 @@ class Crosswater_spiff(Spiff_report):
 	def __init__(self, fname_master, fname_sales):
 		super().__init__(fname_master, fname_sales)
 		self.s_data_crits.append('PN')
+	def extract_codes_from_both(self):
+		for i in range(0, len(self.m_data)):
+			d = self.m_data.get_index(i)
+			temp = d["DESC"]
+			temp = temp.split(' ')
+			temp = temp[1]
+			temp = temp.strip(' ').replace('-', '').replace('_', '')
+			self.m_data.data[i]['Extract_Code'] = temp #will have to fix this once an Dict_lst a modify method, users shouldn't be able to access data directly
+	    #@TODO: implement transformations for sales spiff code data 
+
 
 	def phase_alpha(self):
 		#initial matches
@@ -12,6 +22,7 @@ class Crosswater_spiff(Spiff_report):
 			pcode = self.s_data[i]['Product Code']
 			res = self.m_data.mult_search(['CAT#', 'USER2', 'ALT1'], pcode)
 			self.s_data[i]["PN"] = ''
+
 			self.s_data[i]['Phase'] = ''
 			if res:
 				self.s_data[i]["PN"] = self.m_data.get_index(res)['PN#']
@@ -21,7 +32,7 @@ class Crosswater_spiff(Spiff_report):
 
 
 			if not res:
-				res = self.m_data.mult_search(['CAT#', 'USER2', 'ALT1'], self.trans_pc(pcode))
+				res = self.m_data.mult_search(['CAT#', 'USER2', 'ALT1', 'Extract_Code'], self.trans_pc(pcode))
 				if res:
 					self.s_data[i]["PN"] = self.m_data.get_index(res)['PN#']
 					self.s_data[i]["Phase"] = '0'
@@ -45,7 +56,9 @@ class Crosswater_spiff(Spiff_report):
 	def cat_extract(self, desc):
 		return 
 	def trans_pc(self, pcode):
-		return pcode.replace('-', '')
+		pcode = pcode.replace('-', '')
+		pcode = pcode.replace('_', '')
+		return pcode
 	def is_in(self, d, key, val):
 		if val in d[key]: return True
 		return False
@@ -53,6 +66,7 @@ class Crosswater_spiff(Spiff_report):
 
 
 m_inst = Crosswater_spiff('crosswater_master.csv', 'cwater_sales.csv')
+m_inst.extract_codes_from_both()
 m_inst.phase_alpha()
 m_inst.phase_one()
 m_inst.reintegrate()
