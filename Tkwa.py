9da2840c3a6a4f28226966a6ef245fac75512617
awaddell77 <waddell.andrew@gmail.com>
2019-06-17 11:43:33 -0700

    Update
    Fixed issue with program adding buy/price line onto the subtotal line

--- Tkwa.py
+++ Tkwa.py
@@ -10,14 +10,18 @@ class Tkwa:
 	def process(self, header_start=0):
 		header = self.data.get_header(header_start)
 		target_index = ''
+		targ_col = 'Product........................'
 		for i in range(0, len(header)):
-			if header[i] == 'Product........................': target_index = i
+			if header[i] == targ_col or targ_col in header[i]: target_index = i
+		if not target_index: raise RuntimeError("Could not find Product column")
 		col = self.data.col_grab(target_index)
 		next_start = header_start+1
 		for i in range(0, len(col)):
+			#reads through cells
 			if "Total for" in col[i]:
+				#grabs the name of the buy/price line
 				name = self.comp_name_grab(col[i])
-				self.fills.append([name, next_start, i])
+				self.fills.append([name, next_start, i-1])
 				next_start = i + 2
 		for sects in self.fills:
 			self.data.fill_column(0, sects[0], sects[1], sects[2])
@@ -38,5 +42,6 @@ if __name__ == "__main__":
 	else:
 		m_inst = Tkwa(sys.argv[1])
 		m_inst.process()
+		#cn for custom name
 		if '-cn' in sys.argv: m_inst.export(sys.argv[sys.argv.index('-cn') + 1])
 		else: m_inst.export()
